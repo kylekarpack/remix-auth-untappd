886d3068200f45e543a714d8d2c70291e1438269
Kyle Karpack <kylekarpack@gmail.com>
2022-03-03 08:01:22 -0800

    Basic strategy in place

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,100 @@
-import { SessionStorage } from "@remix-run/server-runtime";
+import { StrategyVerifyCallback } from "remix-auth";
 import {
-  AuthenticateOptions,
-  Strategy,
-  StrategyVerifyCallback,
-} from "remix-auth";
+  OAuth2Profile,
+  OAuth2Strategy,
+  OAuth2StrategyOptions,
+  OAuth2StrategyVerifyParams,
+} from "remix-auth-oauth2";
+import { UntappdUserResponse } from "./types";
 
 /**
  * This interface declares what configuration the strategy needs from the
  * developer to correctly work.
  */
-export interface MyStrategyOptions {
-  something: "You may need";
-}
+export type UntappdStrategyOptions = Pick<
+  OAuth2StrategyOptions,
+  "clientID" | "clientSecret" | "callbackURL"
+>;
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
-}
+export class UntappdStrategy<User> extends OAuth2Strategy<User, OAuth2Profile> {
+  name = "untappd";
 
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
+  private readonly userProfileUrl =
+    "https://api.untappd.com/v4/user/info?compact=true";
 
   constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
+    options: Pick<
+      OAuth2StrategyOptions,
+      "clientID" | "clientSecret" | "callbackURL"
+    >,
+    verify: StrategyVerifyCallback<
+      User,
+      OAuth2StrategyVerifyParams<OAuth2Profile>
+    >
   ) {
-    super(verify);
-    // do something with the options here
+    super(
+      {
+        authorizationURL: "https://untappd.com/oauth/authenticate/",
+        tokenURL: "https://untappd.com/oauth/authorize/",
+        clientID: options.clientID,
+        clientSecret: options.clientSecret,
+        callbackURL: options.callbackURL,
+      },
+      verify
+    );
   }
 
-  async authenticate(
-    request: Request,
-    sessionStorage: SessionStorage,
-    options: AuthenticateOptions
-  ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+  async fetchAccessToken(code: string, params: URLSearchParams) {
+    debugger;
+    params.set("client_id", this.clientID);
+    params.set("client_secret", this.clientSecret);
+    if (params.get("grant_type") === "refresh_token") {
+      params.set("refresh_token", code);
+    } else {
+      params.set("code", code);
+    }
+
+    // Add all parameters as query parameters, as required by Untappd
+    const url = new URL(this.tokenURL);
+    for (const [k, v] of params.entries()) {
+      url.searchParams.append(k, v);
+    }
+    let response = await fetch(url.toString(), {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+
+    if (!response.ok) {
+      try {
+        let body = await response.text();
+        throw new Response(body, { status: 401 });
+      } catch (error: any) {
+        throw new Response(error.message, { status: 401 });
+      }
+    }
+    return await this.getAccessToken(response.clone());
+  }
+
+  protected async userProfile(accessToken: string): Promise<OAuth2Profile> {
+    let response = await fetch(this.userProfileUrl, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    let json: UntappdUserResponse = await response.json();
+    const data = json.response.user;
+    debugger;
+    let profile: OAuth2Profile = {
+      provider: "untappd",
+      displayName: data.user_name,
+      id: data.user_name,
+      name: {
+        familyName: data.last_name,
+        givenName: data.first_name,
+      },
+      emails: [{ value: data.settings.email_address }],
+      photos: [{ value: data.user_avatar }],
+    };
+
+    return profile;
   }
 }
---
+++ src/types.ts
@@ -0,0 +1,150 @@
+export interface UntappdUserResponse {
+  response: {
+    user: UntappdUser;
+  };
+}
+
+export interface UntappdUser {
+  uid: number;
+  id: number;
+  user_name: string;
+  first_name: string;
+  last_name: string;
+  user_avatar: string;
+  user_avatar_hd: string;
+  user_cover_photo: string;
+  user_cover_photo_offset: number;
+  is_private: number;
+  location: string;
+  url: string;
+  bio: string;
+  is_supporter: number;
+  relationship: string;
+  untappd_url: string;
+  account_type: string;
+  stats: Stats;
+  recent_brews: RecentBrews;
+  media: Media;
+  contact: UserContact;
+  date_joined: string;
+  settings: Settings;
+}
+
+export interface UserContact {
+  foursquare: number;
+  twitter: string;
+  facebook: number;
+}
+
+export interface Media {
+  count: number;
+  items: MediaItems;
+}
+
+export interface MediaItems {
+  photo_id: number;
+  photo: Photo;
+  created_at: string;
+  checkin_id: number;
+  user: ItemsUser;
+  beer: Beer;
+  brewery: Brewery;
+  venue: any[];
+}
+
+export interface Beer {
+  bid: number;
+  beer_name: string;
+  beer_label: string;
+  beer_abv: number;
+  beer_style: string;
+  beer_description: string;
+  auth_rating: number;
+  wish_list: boolean;
+}
+
+export interface Brewery {
+  brewery_id: number;
+  brewery_name: string;
+  brewery_slug: string;
+  brewery_label: string;
+  country_name: string;
+  contact: BreweryContact;
+  location: Location;
+  brewery_active: number;
+}
+
+export interface BreweryContact {
+  twitter: string;
+  facebook: string;
+  instagram: string;
+  url: string;
+}
+
+export interface Location {
+  brewery_city: string;
+  brewery_state: string;
+  lat: number;
+  lng: number;
+}
+
+export interface Photo {
+  photo_img_sm: string;
+  photo_img_md: string;
+  photo_img_lg: string;
+  photo_img_og: string;
+}
+
+export interface ItemsUser {
+  uid: number;
+  user_name: string;
+  location: string;
+  bio: string;
+  first_name: string;
+  last_name: string;
+  user_avatar: string;
+  account_type: string;
+  url: string;
+}
+
+export interface RecentBrews {
+  count: number;
+  items: RecentBrewsItems;
+}
+
+export interface RecentBrewsItems {
+  beer: Beer;
+  brewery: Brewery;
+}
+
+export interface Settings {
+  badge: Badge;
+  checkin: Checkin;
+  navigation: Navigation;
+  email_address: string;
+}
+
+export interface Badge {
+  badges_to_facebook: number;
+  badges_to_twitter: number;
+}
+
+export interface Checkin {
+  checkin_to_facebook: number;
+  checkin_to_twitter: number;
+  checkin_to_foursquare: number;
+}
+
+export interface Navigation {
+  default_to_checkin: number;
+}
+
+export interface Stats {
+  total_badges: number;
+  total_friends: number;
+  total_checkins: number;
+  total_beers: number;
+  total_created_beers: number;
+  total_followings: number;
+  total_photos: number;
+}
